d941f36010560bdc4823aa559027a88f1d1e04fb
Ibrahim Fuad <i.fuad@student.unsw.edu.au>
2023-05-17 19:34:01 +1000

    split hypr conf bindings, add neovide config

--- .config/nvchad/init.lua
+++ .config/nvchad/init.lua
@@ -5,3 +5,13 @@
 --   pattern = "*",
 --   command = "tabdo wincmd =",
 -- })
+
+if vim.g.neovide == true then
+  vim.o.guifont = "FiraCode Nerd Font:h14"
+
+  vim.g.neovide_transparency = 0.95
+  vim.g.transparency = 0.95
+
+  vim.g.neovide_floating_blur_amount_x = 2.0
+  vim.g.neovide_floating_blur_amount_y = 2.0
+end
--- .config/nvchad/mappings.lua
+++ .config/nvchad/mappings.lua
@@ -26,4 +26,15 @@ M.minialign = {
   },
 }
 
+-- neovide bindings
+if vim.g.neovide then
+  M.neovide = {
+    n = {
+      ["<C-=>"] = {":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1<CR>", "Neovide zoom in"},
+      ["<C-->"] = {":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1<CR>", "Neovide zoom out"},
+      ["<C-0>"] = {":lua vim.g.neovide_scale_factor = 1<CR>", "Neovide reset zoom"},
+    },
+  }
+end
+
 return M
