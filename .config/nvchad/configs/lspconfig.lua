7112246871ab999826ef1cd856e65d197ae02db6
Ibrahim Fuad <creativeibi77@gmail.com>
2023-06-24 15:56:39 +1000

    add more languages to nvim

--- .config/nvchad/configs/lspconfig.lua
+++ .config/nvchad/configs/lspconfig.lua
@@ -4,7 +4,12 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require("lspconfig")
 
 -- if you just want default config for the servers then put them in a table
-local servers = { "html", "cssls", "tsserver", "clangd", "rust_analyzer", "svelte", "pylsp" }
+local servers = {
+    "html", "cssls", "tsserver", "svelte",
+    "clangd", "rust_analyzer",
+    "pylsp", "gdscript",
+    "nixd",
+}
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
