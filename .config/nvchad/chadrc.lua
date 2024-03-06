b1e80f28afc7edfa12199c2c9984c2f111d249d5
Ibrahim Fuad <creativeibi77@gmail.com>
2024-03-06 15:05:28 +1100

    add row:column to statusline (nvim)

--- .config/nvchad/chadrc.lua
+++ .config/nvchad/chadrc.lua
@@ -2,19 +2,32 @@
 local M = {}
 
 -- Path to overriding theme and highlights files
-local highlights = require "custom.highlights"
+local highlights = require("custom.highlights")
 
 M.ui = {
-  theme = "gruvchad",
-  theme_toggle = { "gruvchad", "tokyonight" },
-  transparency = true,
-  hl_override = highlights.override,
-  hl_add = highlights.add,
+	theme = "gruvchad",
+	theme_toggle = { "gruvchad", "tokyonight" },
+	transparency = true,
+	hl_override = highlights.override,
+	hl_add = highlights.add,
+	statusline = {
+		-- theme = "vscode_colored",
+		-- theme = "default",
+		-- separator_style = "default",
+		overriden_modules = function(modules)
+			local cursor_position = function()
+				local left_sep = "%#St_pos_sep#" .. "" .. "%#St_pos_icon#" .. " "
+				return left_sep .. "%#St_pos_text#" .. " " .. "%l:%c" .. " "
+			end
+			table.remove(modules, 10)
+			table.insert(modules, cursor_position())
+		end,
+	},
 }
 
 M.plugins = "custom.plugins"
 
 -- check core.mappings for table structure
-M.mappings = require "custom.mappings"
+M.mappings = require("custom.mappings")
 
 return M
