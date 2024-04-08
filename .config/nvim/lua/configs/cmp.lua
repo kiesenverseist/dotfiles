c6834ab5155427668db606bcb419b3bdb44d4265
Ibrahim Fuad <i.fuad@student.unsw.edu.au>
2024-04-08 10:21:29 +1000

    forgot to add cmp.lua

---
+++ .config/nvim/lua/configs/cmp.lua
@@ -0,0 +1,38 @@
+local cmp = require "cmp"
+
+local custom = {
+  mapping = {
+    ["<C-p>"] = cmp.mapping.select_prev_item(),
+    ["<C-n>"] = cmp.mapping.select_next_item(),
+    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-e>"] = cmp.mapping.close(),
+    ["<C-y>"] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Insert,
+      select = true,
+    },
+
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if require("luasnip").expand_or_jumpable() then
+        vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if require("luasnip").jumpable(-1) then
+        vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+  },
+}
+
+local base = require "nvchad.configs.cmp"
+
+base.mapping = nil
+
+return vim.tbl_deep_extend("force", base, custom)
