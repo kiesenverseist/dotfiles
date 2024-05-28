a6e69bec371c34a307464e618392e2cd9d90082b
Ibrahim Fuad <creativeibi77@gmail.com>
2024-05-28 14:18:17 +1000

    consolidate pkgs

---
+++ .config/system/home/packages/gdlauncher.nix
@@ -0,0 +1,25 @@
+{pkgs}:
+let 
+  name = "gdlauncher";
+  src = pkgs.fetchurl {
+    url = "https://github.com/gorilla-devs/GDLauncher/releases/download/v1.1.30/GDLauncher-linux-setup.AppImage";
+    sha256 = "00rzv0i4g701v9ssy55lg7sjddrgfnf0mdq5x85c1831xkfx7ig1";
+  };
+  # exec = pkgs.appimageTools.wrapType1 { inherit name src; };
+  contents = pkgs.appimageTools.extractType2 {inherit name src;};
+in 
+pkgs.appimageTools.wrapType2 {
+  inherit name src;
+
+  extraInstallCommands = ''
+    install -Dm444 ${contents}/${name}.desktop -t $out/share/applications
+    install -Dm444 ${contents}/${name}.png -t $out/share/pixmaps
+    substituteInPlace $out/share/applications/${name}.desktop \
+      --replace 'Exec=AppRun --no-sandbox %U' 'Exec=${name} --no-sandbox'
+  '';
+}
+  # desktop = pkgs.makeDesktopItem {
+  #   name = "gdlauncher";
+  #   desktopName = "GDLauncher";
+  #   exec = "gdlauncher --no-sandbox";
+  # };
---
+++ .config/system/home/packages/lmms.nix
@@ -0,0 +1,41 @@
+{pkgs}:
+let 
+  name = "lmms";
+  src = pkgs.requireFile {
+    name = "lmms-1.3.0-alpha.1.431+gca9e98959-linux-x86_64.AppImage";
+    url = "https://github.com/LMMS/lmms/suites/16996172402/artifacts/970795165";
+    sha256 = "c65d47f80f2d3e1751e401e583029d9aeafe50d789c0e762dd90455fd63f45eb";
+  };
+  # exec = pkgs.appimageTools.wrapType1 { inherit name src; };
+  contents = pkgs.appimageTools.extractType1 {inherit name src;};
+in 
+pkgs.appimageTools.wrapType1 {
+  inherit name src;
+
+  extraPkgs = pkgs: with pkgs; [
+    carla
+    alsa-lib
+    fftwFloat
+    fltk13
+    fluidsynth
+    lame
+    libgig
+    libjack2
+    libpulseaudio
+    libsamplerate
+    libsndfile
+    libsoundio
+    libvorbis
+    portaudio
+    libsForQt5.qt5.qtbase
+    libsForQt5.qt5.qtx11extras
+    SDL2
+  ];
+
+  extraInstallCommands = ''
+    install -Dm444 ${contents}/${name}.desktop -t $out/share/applications
+    install -Dm444 ${contents}/${name}.png -t $out/share/pixmaps
+    substituteInPlace $out/share/applications/${name}.desktop \
+      --replace 'Exec=lmms.real' 'Exec=env QT_QPA_PLATFORM="xcb" lmms.real'
+  '';
+}
