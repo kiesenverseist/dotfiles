69bbc1590dbd7636f53582a3fb28a1fc75b3f67e
Ibrahim Fuad <creativeibi77@gmail.com>
2024-09-23 23:38:48 +1000

    nvidia sorta fixes

--- .config/system/hosts/graphite/nvidia.nix
+++ .config/system/hosts/graphite/nvidia.nix
@@ -1,38 +1,38 @@
 {lib, config, ...}: {
-  config = lib.mkIf (!config.vfio.enable) {
+  config = lib.mkIf (! config.vfio.enable) {
     hardware.nvidia = {
       modesetting.enable = true;
       open = false;
       nvidiaSettings = true;
-      prime = {
-        sync.enable = true;        
-        amdgpuBusId = "PCI:12:0:0";
-        nvidiaBusId = "PCI:1:0:0";
-      };
+      # prime = {
+      #   sync.enable = true;        
+      #   amdgpuBusId = "PCI:11:0:0";
+      #   nvidiaBusId = "PCI:1:0:0";
+      # };
     };
 
     # services.xserver.enable = lib.mkForce false;
 
-    services.xserver.videoDrivers = lib.mkForce ["nvidia" "amdgpu"];
+    services.xserver.videoDrivers = lib.mkForce ["nvidia"];
 
-    virtualisation.libvirtd.enable = lib.mkForce false;
+    # virtualisation.libvirtd.enable = lib.mkForce false;
 
-    # environment.variables = {
-    #   LIBVA_DRIVER_NAME = "nvidia";
-    #   XDG_SESSION_TYPE = "wayland";
-    #   GBM_BACKEND = "nvidia-drm";
-    #   __GLX_VENDOR_LIBRARY_NAME = "nvidia";
-    # }; 
+    environment.variables = {
+      LIBVA_DRIVER_NAME = "nvidia";
+      XDG_SESSION_TYPE = "wayland";
+      GBM_BACKEND = "nvidia-drm";
+      __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+    }; 
 
-    # boot.blacklistedKernelModules = [
-    #   "amdgpu"
-    # ];
+    boot.blacklistedKernelModules = [
+      "amdgpu"
+    ];
 
-    # boot.kernelParams = [
-    #   "initcall_blacklist=simpledrm_platform_driver_init"
-    #   "nvidia-drm.modeset=1"
-    #   "nvidia-drm.fbdev=1"
-    # ];
+    boot.kernelParams = [
+      "initcall_blacklist=simpledrm_platform_driver_init"
+      "nvidia-drm.modeset=1"
+      "nvidia-drm.fbdev=1"
+    ];
 
   };
 }
