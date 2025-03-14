88fad8f53aa349c83315e5a276be2779cf013562
Ibrahim Fuad <creativeibi77@gmail.com>
2025-03-15 00:42:07 +1100

    Refactor home default nix

--- home/default.nix
+++ home/default.nix
@@ -2,32 +2,21 @@
   inputs,
   pkgs,
 }: let
-  extraSpecialArgs = {inherit inputs;};
-  conf = attrs:
-    inputs.home-manager.lib.homeManagerConfiguration (
-      {inherit pkgs extraSpecialArgs;} // attrs
-    );
-  commonModules = [inputs.stylix.homeManagerModules.stylix ./modules];
+  conf = module:
+    inputs.home-manager.lib.homeManagerConfiguration {
+      inherit pkgs;
+      extraSpecialArgs = {inherit inputs;};
+      modules = [
+        module
+        inputs.stylix.homeManagerModules.stylix
+        ./modules
+      ];
+    };
 in {
-  "kiesen@halite" = conf {
-    modules = [./home-halite.nix] ++ commonModules;
-
-    extraSpecialArgs =
-      extraSpecialArgs // {nix-gaming = inputs.nix-gaming;};
-  };
-  "kiesen@graphite" = conf {
-    modules = [./home-graphite.nix] ++ commonModules;
-  };
-  "kiesen@kiesen-eos-laptop" = conf {
-    modules = [./home-laptop.nix] ++ commonModules;
-  };
-  "kiesen@fluorite" = conf {
-    modules = [./home-fluorite.nix] ++ commonModules;
-  };
-  "ibrahim.fuad@au-lap-0618.saberastronautics.net" = conf {
-    modules = [./work-laptop.nix] ++ commonModules;
-  };
-  "ibrahim.fuad@graphite" = conf {
-    modules = [./work-graphite.nix] ++ commonModules;
-  };
+  "kiesen@halite" = conf ./home-halite.nix;
+  "kiesen@graphite" = conf ./home-graphite.nix;
+  "kiesen@kiesen-eos-laptop" = conf ./home-laptop.nix;
+  "kiesen@fluorite" = conf ./home-fluorite.nix;
+  "ibrahim.fuad@au-lap-0618.saberastronautics.net" = conf ./work-laptop.nix;
+  "ibrahim.fuad@graphite" = conf ./work-graphite.nix;
 }
