47c13626b35182fdbfd1832c4bf9c3d19dd5d0e7
Ibrahim Fuad <creativeibi77@gmail.com>
2025-07-31 19:44:18 +1000

    switch back to unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "My system configuration flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.05";
 
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
