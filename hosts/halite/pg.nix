904ee95efe6375f838407162718500e200ad40e7
Ibrahim Fuad <creativeibi77@gmail.com>
2025-08-13 14:04:16 +1000

    lock halite db version

--- hosts/halite/pg.nix
+++ hosts/halite/pg.nix
@@ -1,18 +1,22 @@
-{config, pkgs, ...}: {
-
+{
+  config,
+  pkgs,
+  ...
+}: {
   services.postgresql = {
     enable = true;
-	authentication = pkgs.lib.mkOverride 10 ''
-	  #type	database	DBuser	auth-method
-	  local	all			all		trust
-	'';
+    package = pkgs.postgresql_16;
+    authentication = pkgs.lib.mkOverride 10 ''
+      #type	database	DBuser	auth-method
+      local	all			all		trust
+    '';
   };
 
   services.pgadmin = {
     enable = true;
-	openFirewall = true;
-	initialEmail = "creativeibi77@gmail.com";
-	initialPasswordFile = config.sops.secrets."pgadminPassword".path;
+    openFirewall = true;
+    initialEmail = "creativeibi77@gmail.com";
+    initialPasswordFile = config.sops.secrets."pgadminPassword".path;
   };
 
   sops.secrets."pgadminPassword" = {
