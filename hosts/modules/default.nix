1d9e9dd2e81e009cfdacf4e18786d9299b105a79
Ibrahim Fuad <creativeibi77@gmail.com>
2025-08-13 14:04:24 +1000

    increase default buffer size

--- hosts/modules/default.nix
+++ hosts/modules/default.nix
@@ -50,6 +50,7 @@
 
     nix = {
       settings = {
+        download-buffer-size = 524288000;
         experimental-features = ["nix-command" "flakes"];
         trusted-users = ["root" "@wheel"];
       };
