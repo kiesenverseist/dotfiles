ed79713b9872f86cc2a87a4053e117a20f659605
Ibrahim Fuad <creativeibi77@gmail.com>
2025-09-29 22:36:58 +1000

    make infra for gleam

--- config/nvim/after/ftplugin/gleam.lua
+++ config/nvim/after/ftplugin/gleam.lua
@@ -3,3 +3,11 @@ vim.opt_local.expandtab = true
 vim.opt_local.tabstop = 2
 vim.opt_local.softtabstop = 2
 vim.opt_local.shiftwidth = 2
+vim.opt.errorformat = {
+	"%Wwarning: %m", -- start-of-warning
+	"%Eerror: %m", -- start-of-error
+	"%C%*[^┌]┌─ %f:%l:%c", -- continuation: boxed, pipe-separated
+	"%C%*[^┌]│", -- ignore the vertical bar-only line
+	"%-G%.%#", -- ignore the rest
+}
+vim.opt.makeprg = "cd %:h && gleam build"
