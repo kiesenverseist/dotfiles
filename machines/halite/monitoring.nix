01df541dd176b075dee6ba04e0269b15af2518e3
Ibrahim Fuad <creativeibi77@gmail.com>
2026-04-01 21:40:11 +1100

    fix: use clan to generate grafana secret

--- machines/halite/monitoring.nix
+++ machines/halite/monitoring.nix
@@ -1,9 +1,16 @@
-{config, ...}: {
+{
+  config,
+  pkgs,
+  ...
+}: {
   services.grafana = {
     enable = true;
-    settings.server = {
-      domain = "grafana.kiesen.moe";
-      root_url = "https://grafana.kiesen.moe";
+    settings = {
+      server = {
+        domain = "grafana.kiesen.moe";
+        root_url = "https://grafana.kiesen.moe";
+      };
+      security.secret_key = "$__file{${config.clan.core.vars.generators.grafana.files.key.path}}";
     };
 
     provision = {
@@ -20,11 +27,19 @@
     };
   };
 
+  clan.core.vars.generators.grafana = {
+    files.key.secret = true;
+    files.key.group = "grafana";
+    files.key.mode = "0440";
+
+    script = "${pkgs.openssl}/bin/openssl rand -base64 32 > $out/key";
+  };
+
   services.caddy.virtualHosts = let
     inherit (config.services.grafana.settings.server) domain http_addr http_port;
   in {
     ${domain}.extraConfig = ''
-      reverse_proxy http://${http_addr}:${builtins.toString http_port}
+      reverse_proxy http://${http_addr}:${toString http_port}
       # import porkbun
     '';
   };
