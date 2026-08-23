49f1a0a4c57816b0c73f5df89544add3ac20c243
Ibrahim Fuad <creativeibi77@gmail.com>
2026-08-23 11:32:20 +1000

    feat(infra): cockroachdb

---
+++ modules/services/cockroachdb.nix
@@ -0,0 +1,81 @@
+{...}: {
+
+  clan.inventory.instances.cockroachdb = {
+    module.input = "self";
+    module.name = "@kiesen/cockroachdb";
+    roles.default.machines.graphite = {};
+  };
+
+  clan.modules."@kiesen/cockroachdb" = {
+    _class = "clan.service";
+
+    manifest = {
+      name = "cockroachdb";
+      description = "A distributed HA postgres compatible database";
+      categories = ["System"];
+    };
+
+    roles.default = {
+      description = "To include this machine as a node in the database cluster";
+      interface = {...}: {
+        options = {
+        };
+      };
+
+      perInstance = {settings, ...}: {
+        nixosModule = {
+          config,
+          lib,
+          pkgs,
+          ...
+        }: let
+          pkgsUnfree = import pkgs.path {
+            inherit (pkgs.stdenv.hostPlatform) system;
+            config.allowUnfreePackages = ["cockroachdb"];
+          };
+        in {
+          nixpkgs.config.allowUnfree = true;
+  
+          clan.core.vars.generators = {
+            cockroachdb-ca = {
+              share = true;
+              files."ca.crt" = { };
+              files."ca.key".deploy = false;
+              runtimeInputs = [ pkgsUnfree.cockroachdb ];
+              script = ''
+                cockroachdb cert create-ca \
+                  --certs-dir="$out" \
+                  --ca-key="$out"/ca.key
+              '';
+            };
+            cockroachdb-node = {
+              files."ca.crt" = { };
+              files."node.crt" = { };
+              files."node.key" = { };
+
+              dependencies = ["cockroachdb-ca"];
+              runtimeInputs = [ pkgsUnfree.cockroachdb ];
+
+              script = ''
+                cp "$in"/cockroachdb-ca/ca.crt "$out"/ca.crt
+                cockroachdb cert create-node \
+                  localhost \
+                  ${config.networking.hostName} \
+                  --certs-dir="$out" \
+                  --ca-key="$in"/cockroachdb-ca/ca.key
+              '';
+            };
+          };
+
+          services.cockroachdb = {
+            enable = true;
+            # insecure = true;
+            certsDir = "/run/secrets/vars/per-machine/${config.networking.hostName}/cockroachdb-node";
+          };
+
+          
+        };
+      };
+    };
+  };
+}
