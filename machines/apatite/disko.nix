53538d8efe253a4bbcf080309430b939139f952c
Ibrahim Fuad <creativeibi77@gmail.com>
2026-08-29 18:11:27 +1000

    machines/apatite: set disk schema to ext4-single-disk

---
+++ machines/apatite/disko.nix
@@ -0,0 +1,51 @@
+# ---
+# schema = "ext4-single-disk"
+# [placeholders]
+# mainDisk = "/dev/disk/by-id/scsi-36053fecb49ef49ef98da3991ca36c132" 
+# ---
+# This file was automatically generated!
+# CHANGING this configuration requires wiping and reinstalling the machine
+{
+  boot.loader.grub = {
+    efiInstallAsRemovable = true;
+    efiSupport = true;
+  };
+
+  disko.devices = {
+    disk = {
+      main = {
+        name = "main-82b0fd48341b4fed999315a7c44d5efa";
+        device = "/dev/disk/by-id/scsi-36053fecb49ef49ef98da3991ca36c132";
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            "boot" = {
+              size = "1M";
+              type = "EF02"; # for grub MBR
+              priority = 1;
+            };
+            ESP = {
+              type = "EF00";
+              size = "500M";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
