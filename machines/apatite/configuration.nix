2473a773ea7295008464de6dbee27420f27c26fe
Ibrahim Fuad <creativeibi77@gmail.com>
2026-08-30 14:28:26 +1000

    feat(infra): add apatite config

--- machines/apatite/configuration.nix
+++ machines/apatite/configuration.nix
@@ -1,7 +1,46 @@
-{
-  imports = [
+{pkgs, ...}: {
 
+  boot.tmp.cleanOnBoot = true;
+  zramSwap.enable = true;
+  time.timeZone = "Australia/Sydney";
+
+  users.users.root.openssh.authorizedKeys.keys = [''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILB44rGxgd27wPLkuUrHXlnrpEhqVQX92k1F3TVNYIWQ kiesen@graphite''];
+
+  users.users.kiesen = {
+    isNormalUser = true;
+    shell = pkgs.fish;
+    extraGroups = ["wheel"];
+
+    openssh.authorizedKeys.keys = [''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILB44rGxgd27wPLkuUrHXlnrpEhqVQX92k1F3TVNYIWQ kiesen@graphite''];
+  };
+
+  nixpkgs.config.allowUnfree = true;
+  nixpkgs.hostPlatform = "x86_64-linux";
+  nix.settings.experimental-features = ["nix-command" "flakes"];
+
+  environment.systemPackages = [
+    pkgs.wget
+    pkgs.curl
+    pkgs.btop
   ];
 
-  # New machine!
+  # Programs
+  programs.fish.enable = true;
+  programs.git.enable = true;
+  programs.tmux.enable = true;
+  programs.neovim.defaultEditor = true;
+  programs.htop.enable = true;
+  programs.nh = {
+    enable = true;
+    clean.enable = true;
+  };
+
+  # Serivces
+  services.openssh.enable = true;
+
+  services.tailscale = {
+    enable = true;
+    # permitCertUid = "caddy";
+  };
+
 }
